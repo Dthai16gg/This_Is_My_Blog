118aa5b0c1f35e4cd6ae7031eaadb713b51d016b
Doanh Thái Bùi <88380128+Dthai16gg@users.noreply.github.com>
2023-05-24 01:31:29 +0700

    Add iReponstories

---
+++ DATA/IReponstories/IReponstories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA.IReponstories
+{
+    public interface IReponstories<T>
+    {
+        //crud tra ve public string
+        public string Create(T item);
+        public string Update(T item);
+        public string Delete(T item);
+        //get
+        public IEnumerable<T> GetAll();
+        public T GetByID(int id);
+        public IEnumerable<T> GetByName(string name);
+        //crud many
+        public string CreateMany(List<T> item);
+        public string UpdateMany(List<T> item);
+        public string DeleteMany(List<T> item);
+    }
+}
